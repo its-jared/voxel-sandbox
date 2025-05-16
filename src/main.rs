9dbd714efe96f9c81979f189fb5761efd4abb2c9
Jared <78033286+its-jared@users.noreply.github.com>
2025-05-16 15:37:41 -0400

    _

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,124 @@
-use bevy::prelude::*;
+
+use std::sync::Arc;
+
+use bevy::{pbr::CascadeShadowConfigBuilder, platform::collections::HashMap, prelude::*};
 use bevy_voxel_world::prelude::*;
-use rand::Rng;
+use noise::{HybridMulti, NoiseFn, Perlin};
+
+#[derive(Resource, Clone, Default)]
+struct MainWorld;
+
+impl VoxelWorldConfig for MainWorld {
+    type MaterialIndex = u8;
+    type ChunkUserBundle = ();
+
+    fn spawning_distance(&self) -> u32 {
+        25
+    }
+
+    fn voxel_lookup_delegate(&self) -> VoxelLookupDelegate<Self::MaterialIndex> {
+        Box::new(move |_chunk_pos| get_voxel_fn())
+    }
+
+    fn texture_index_mapper(
+        &self,
+    ) -> Arc<dyn Fn(Self::MaterialIndex) -> [u32; 3] + Send + Sync> {
+        Arc::new(|mat| match mat {
+            0 => [0, 0, 0],
+            1 => [1, 1, 1],
+            2 => [2, 2, 2],
+            3 => [3, 3, 3],
+            _ => [0, 0, 0],
+        })
+    }
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(VoxelWorldPlugin::default())
+        .add_plugins(VoxelWorldPlugin::with_config(MainWorld))
         .add_systems(Startup, setup)
-        .add_systems(Update, (set_solid_voxel, move_camera))
         .run();
 }
 
 fn setup(mut commands: Commands) {
-    // Camera
+    // camera
     commands.spawn((
         Camera3d::default(),
-        Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        Transform::from_xyz(-200.0, 180.0, -200.0).looking_at(Vec3::ZERO, Vec3::Y),
         // This tells bevy_voxel_world to use this cameras transform to calculate spawning area
-        VoxelWorldCamera::<DefaultWorld>::default(),
+        VoxelWorldCamera::<MainWorld>::default(),
     ));
 
-    // Ambient light
+    // Sun
+    let cascade_shadow_config = CascadeShadowConfigBuilder { ..default() }.build();
+    commands.spawn((
+        DirectionalLight {
+            color: Color::srgb(0.98, 0.95, 0.82),
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, 0.0)
+            .looking_at(Vec3::new(-0.15, -0.1, 0.15), Vec3::Y),
+        cascade_shadow_config,
+    ));
+
+    // Ambient light, same color as sun
     commands.insert_resource(AmbientLight {
         color: Color::srgb(0.98, 0.95, 0.82),
-        brightness: 1000.0,
+        brightness: 100.0,
         affects_lightmapped_meshes: true,
     });
 }
 
-fn set_solid_voxel(mut voxel_world: VoxelWorld<DefaultWorld>) {
-    // Generate some random values
-    let size = 10;
-    let mut rng = rand::rng();
-    let x = rng.random_range(-size..size);
-    let y = rng.random_range(-size..size);
-    let z = rng.random_range(-size..size);
-    let voxel_type = rng.random_range(0..4);
-    let pos = IVec3::new(x, y, z);
+fn get_voxel_fn() -> Box<dyn FnMut(IVec3) -> WorldVoxel + Send + Sync> {
+    // Set up some noise to use as the terrain height map
+    let mut noise = HybridMulti::<Perlin>::new(1234);
+    noise.octaves = 5;
+    noise.frequency = 1.1;
+    noise.lacunarity = 2.8;
+    noise.persistence = 0.4;
 
-    // Set a voxel at the random position with the random type
-    if pos.distance_squared(IVec3::ZERO) < i32::pow(size, 2) {
-        voxel_world.set_voxel(pos, WorldVoxel::Solid(voxel_type));
-    }
+    // We use this to cache the noise value for each y column so we only need
+    // to calculate it once per x/z coordinate
+    let mut cache = HashMap::<(i32, i32), f64>::new();
+
+    // Then we return this boxed closure that captures the noise and the cache
+    // This will get sent off to a separate thread for meshing by bevy_voxel_world
+    Box::new(move |pos: IVec3| {
+        // Sea level
+        if pos.y < 1 {
+            return WorldVoxel::Solid(3);
+        }
+
+        let [x, y, z] = pos.as_dvec3().to_array();
+
+        // If y is less than the noise sample, we will set the voxel to solid
+        let is_ground = y < match cache.get(&(pos.x, pos.z)) {
+            Some(sample) => *sample,
+            None => {
+                let sample = noise.get([x / 1000.0, z / 1000.0]) * 50.0;
+                cache.insert((pos.x, pos.z), sample);
+                sample
+            }
+        };
+
+        if is_ground {
+            // Solid voxel of material type 0
+            WorldVoxel::Solid(0)
+        } else {
+            WorldVoxel::Air
+        }
+    })
 }
 
-// Rotate the camera around the origin
 fn move_camera(
     time: Res<Time>,
-    mut query: Query<&mut Transform, With<VoxelWorldCamera<DefaultWorld>>>,
+    mut cam_transform: Query<&mut Transform, With<VoxelWorldCamera<MainWorld>>>,
 ) {
-    let mut transform: Mut<'_, Transform> = query.single_mut().unwrap();
-    let time_seconds = time.elapsed_secs();
-    transform.translation.x = 25.0 * (time_seconds * 0.1).sin();
-    transform.translation.z = 25.0 * (time_seconds * 0.1).cos();
-    transform.look_at(Vec3::ZERO, Vec3::Y);
+    let Ok(mut transform) = cam_transform.single_mut() else {
+        return;
+    };
+    transform.translation.x += time.delta_secs() * 25.0;
+    transform.translation.z += time.delta_secs() * 30.0;
 }
